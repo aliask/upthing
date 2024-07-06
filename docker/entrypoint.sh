0b3e3608371f9238d35fb84b700dbe0c5cfc6a70
Will Robertson <6219869+aliask@users.noreply.github.com>
2024-07-06 14:56:45 +1000

    Use postgres healthcheck instead of custom script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-wait-for-it.sh "${DB_HOST}:${DB_PORT}"
-
-if [ $? -ne 0 ]; then
-  logger --stderr --priority daemon.warn "waiting for the database to be ready timed out. There may be connection issues." 
-fi
-
 cron -f &
 
 php artisan key:generate
