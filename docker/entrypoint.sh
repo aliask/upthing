db6c1eacb19251247f198823386578bfcf3f3747
Will Robertson <6219869+aliask@users.noreply.github.com>
2024-07-06 14:56:44 +1000

    Dockerised deployment

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+wait-for-it.sh "${DB_HOST}:${DB_PORT}"
+
+if [ $? -ne 0 ]; then
+  logger --stderr --priority daemon.warn "waiting for the database to be ready timed out. There may be connection issues." 
+fi
+
+cron -f &
+
+php artisan key:generate
+php artisan migrate:refresh --seed
+
+docker-php-entrypoint php-fpm
